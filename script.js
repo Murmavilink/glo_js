0941ca7f1f4247d8105994e72aabf43d0e1ec652
Mur <murmavilink@gmail.com>
2022-11-25 15:59:11 +0600

    добавил переменные

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+
+let rollback = 10;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
