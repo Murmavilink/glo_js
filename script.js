4d6748c66e76a81243f2760bd82128c24704d1d4
Mur <murmavilink@gmail.com>
2022-11-26 12:59:19 +0600

    урок 2 выполнено

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
-
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-
 let rollback = 10;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(screens.toLowerCase().split(''));
+console.log(servicePercentPrice);
+
+
+
+
+
