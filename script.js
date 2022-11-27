3f1c067ad2b5d823da3ea480717aba71446fc156
Mur <murmavilink@gmail.com>
2022-11-27 16:26:52 +0600

    урок 3 выполнено

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 'use strict';
 
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
+// let title = prompt('Как называется ваш проект?');
+// let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+// let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+// let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
+// let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+
 let rollback = 10;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+let servicePercentPrice = Math.ceil( fullPrice - (fullPrice * (rollback / 100)) );
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-console.log(screens.length);
+// console.log(title);
+// console.log(adaptive);
+// console.log(screens.length);
 console.log(screenPrice);
-console.log(screens.toLowerCase().split(''));
-console.log(servicePercentPrice);
+console.log('fullPrice', fullPrice);
+console.log('servicePercentPrice', servicePercentPrice);
 
-
-
-
-
+if(fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if(fullPrice >= 15000 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log("Скидка не предусмотрена");
+} else {
+    alert('Что то пошло не так');
+}
