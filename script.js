5b053ea66d64bf2ecf3e7e2cc61c811285f76340
Mur <murmavilink@gmail.com>
2022-12-02 12:09:30 +0600

    рефакторинг урок 5

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+
+
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-let rollback = 10;
 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
-let allServicePrices;
+// Функция возвращает сумму всех дополнительных услуг
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+ };
 
 
 const showTypeOf = function(variable) {
@@ -21,28 +29,21 @@ const showTypeOf = function(variable) {
 };
 
 
-// Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
-const getTitle = function (titleProject) {
-    let newTitle = titleProject.trim();
-
-    return newTitle[0].toUpperCase() + newTitle.substring(1).toLowerCase();
+// Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
+const getFullPrice = function () {
+    return screenPrice + allServicePrices;
 };
 
 
-// Функция возвращает сумму всех дополнительных услуг
-const getAllServicePrices = function() {
-    return allServicePrices = servicePrice1 + servicePrice2;
- };
-
-// Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
-// function declaration
-function getFullPrice() {
-    return fullPrice = screenPrice + allServicePrices;
-}
-
 // Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
 const getServicePercentPrices = function() {
-    return servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+    return fullPrice - (fullPrice * (rollback / 100));
+};
+
+
+// Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
+const getTitle = function () {
+    return title[0].toUpperCase() + title.trim().substring(1).toLowerCase();
 };
 
 
@@ -61,18 +62,26 @@ const getRollbackMessage = function(price) {
 
 
 
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
+
+
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log( getTitle(title) );
+console.log(getRollbackMessage(fullPrice))
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
 
-console.log( getAllServicePrices() );
-console.log( getFullPrice() );
+console.log(screens.length);
+console.log(servicePercentPrice);
 
-console.log( getServicePercentPrices() );
-console.log( getRollbackMessage(fullPrice) );
 
+console.log("Стоимость верстки экранов " + screenPrice + " тенге и Стоимость разработки сайта " + fullPrice + " тенге");
 
 
 
