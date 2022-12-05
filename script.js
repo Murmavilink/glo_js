f5d277d2979466217161fa4254a8ff065551f36d
Mur <murmavilink@gmail.com>
2022-12-05 11:40:00 +0600

    урок 7 выполнено

--- script.js
+++ script.js
@@ -19,94 +19,111 @@ const appData = {
         
         do {
             appData.screenPrice = prompt('Сколько будет стоить данная работа?');
-        } while(!isNumber(appData.screenPrice));
+        } while(!appData.isNumber(appData.screenPrice));
         
     
         appData.adaptive = confirm('Нужен ли адаптив на сайте?');
     },
 
-};
 
+    isNumber: function(num) {
+        return !isNaN( parseFloat(num) ) && isFinite(num);
+    },
+    
+    
+    
+    // Метод возвращает сумму всех дополнительных услуг
+    getAllServicePrices: function() {
+        let sum = 0;
+    
+        for(let i = 0; i < 2; i++) {
+            let price = 0;
+    
+            if(i === 0) {
+                appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
+            } else if (i === 1) {
+                appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
+            }
+    
+            do{
+                price = prompt('Сколько это будет стоить?');
+            } while(!appData.isNumber(price));
+    
+            
+            sum += +price;
+    
+            // sum += +prompt('Сколько это будет стоить?');
+            // sum = sum + +prompt('Сколько это будет стоить?');
+        }
+        
+        return sum;
+     },
+    
+    
+    
+    // Метод возвращает сумму стоимости верстки и стоимости дополнительных услуг
+    getFullPrice: function () {
+        return +appData.screenPrice + appData.allServicePrices;
+    },
+    
+    
+    // Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
+    getServicePercentPrices: function() {
+        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+    },
+    
+    
+    // Метод возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
+    getTitle: function () {
+        return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase();
+    },
+    
+    
+    
+    getRollbackMessage: function(price) {
+        if(price >= 30000) {
+            return 'Даем скидку в 10%';
+        } else if(price >= 15000 && price < 30000) {
+            return 'Даем скидку в 5%';
+        } else if (price < 15000 && price >= 0) {
+            return "Скидка не предусмотрена";
+        } else {
+            return 'Что то пошло не так';
+        }
+    },
 
-const isNumber = function(num) {
-    return !isNaN( parseFloat(num) ) && isFinite(num);
-};
-
-
-
-// Функция возвращает сумму всех дополнительных услуг
-const getAllServicePrices = function() {
-    let sum = 0;
-
-    for(let i = 0; i < 2; i++) {
-        let price = 0;
-
-        if(i === 0) {
-            appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
-        } else if (i === 1) {
-            appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
+    logger: function() {
+        
+        for(const key in appData) {
+            console.log(key, appData[key]);
         }
 
-        do{
-            price = prompt('Сколько это будет стоить?');
-        } while(!isNumber(price));
+    },
 
-        
-        sum += +price;
+    start: function() {
+        appData.asking();
 
-        // sum += +prompt('Сколько это будет стоить?');
-        // sum = sum + +prompt('Сколько это будет стоить?');
-    }
-    
-    return sum;
- };
+        appData.allServicePrices = appData.getAllServicePrices();
+        appData.fullPrice = appData.getFullPrice();
+        appData.servicePercentPrice = appData.getServicePercentPrices();
+        appData.title = appData.getTitle();
 
+        appData.logger();
+    },
 
-
-
-
-// Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
-const getFullPrice = function () {
-    return +appData.screenPrice + appData.allServicePrices;
-};
-
-
-// Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
-const getServicePercentPrices = function() {
-    return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-};
-
-
-// Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
-const getTitle = function () {
-    return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase();
 };
 
 
 
-const getRollbackMessage = function(price) {
-    if(price >= 30000) {
-        return 'Даем скидку в 10%';
-    } else if(price >= 15000 && price < 30000) {
-        return 'Даем скидку в 5%';
-    } else if (price < 15000 && price >= 0) {
-        return "Скидка не предусмотрена";
-    } else {
-        return 'Что то пошло не так';
-    }
-};
-
-
-appData.asking();
-appData.allServicePrices = getAllServicePrices();
-appData.fullPrice = getFullPrice();
-appData.servicePercentPrice = getServicePercentPrices();
-appData.title = getTitle();
+appData.start();
 
 
 
-console.log(appData.fullPrice);
-console.log(appData.servicePercentPrice);
+
+
+
+// console.log(appData.fullPrice);
+// console.log(appData.servicePercentPrice);
 
 // console.log('////////////////////////////////////////');
 // console.log(title);
