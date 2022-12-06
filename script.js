d51ec54e18bcbe039bd6518a29eebb8b876f481a
Mur <murmavilink@gmail.com>
2022-12-06 14:38:07 +0600

    урок 8 выполнено

--- script.js
+++ script.js
@@ -3,23 +3,21 @@
 
 const appData = {
     title: '',
-    screens: '',
+    screens: [],
     screenPrice: 0, 
     adaptive: true,
     rollback: 10,
     allServicePrices: 0,
     fullPrice: 0,
     servicePercentPrice: 0,
-    service1: '',
-    service2: '',
+    services: {},
 
     start: function() {
         appData.asking();
-
-        appData.allServicePrices = appData.getAllServicePrices();
-        appData.fullPrice = appData.getFullPrice();
-        appData.servicePercentPrice = appData.getServicePercentPrices();
-        appData.title = appData.getTitle();
+        appData.addPrices();
+        appData.getFullPrice();
+        appData.getServicePercentPrices();
+        appData.getTitle();
 
         appData.logger();
     },
@@ -30,59 +28,69 @@ const appData = {
 
     asking: function() {
         appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-        appData.screens = prompt('Какие типы экранов нужно разработать?', 'Порстые, Сложные');
+        // appData.screens = prompt('Какие типы экранов нужно разработать?', 'Порстые, Сложные');
         
-        do {
-            appData.screenPrice = prompt('Сколько будет стоить данная работа?');
-        } while(!appData.isNumber(appData.screenPrice));
-        
-    
-        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-    },
+        // do {
+        //     appData.screenPrice = prompt('Сколько будет стоить данная работа?');
+        // } while(!appData.isNumber(appData.screenPrice));
 
-    
-    // Метод возвращает сумму всех дополнительных услуг
-    getAllServicePrices: function() {
-        let sum = 0;
-    
+
         for(let i = 0; i < 2; i++) {
+            let name = prompt('Какие типы экранов нужно разработать?');
             let price = 0;
-    
-            if(i === 0) {
-                appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
-            } else if (i === 1) {
-                appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
-            }
+
+            do {
+                price = prompt('Сколько будет стоить данная работа?');
+            } while(!appData.isNumber(price));
+
+            appData.screens.push({
+                id: i,
+                name: name,
+                price: price
+            });
+        }
+
+
+        for(let i = 0; i < 2; i++) {
+            let name = prompt('Какой дополнительный тип услуги нужен?');
+            let price = 0;
     
             do{
                 price = prompt('Сколько это будет стоить?');
             } while(!appData.isNumber(price));
-    
-            
-            sum += +price;
 
+            appData.services[name] = +price;
         }
-        
-        return sum;
-     },
-    
+
+        appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+    },
+
+    addPrices: function() {
+        for (const screen of appData.screens) {
+            appData.screenPrice += +screen.price;
+        }
+
+        for(let key in appData.services) {
+            appData.allServicePrices += appData.services[key];
+        }
+    },
     
     
     // Метод возвращает сумму стоимости верстки и стоимости дополнительных услуг
     getFullPrice: function () {
-        return +appData.screenPrice + appData.allServicePrices;
+        appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
     },
     
     
     // Метод возвращает сумму стоимости верстки и стоимости дополнительных услуг
     getServicePercentPrices: function() {
-        return appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
     },
     
     
     // Метод возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой"
     getTitle: function () {
-        return appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase();
+        appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase();
     },
     
     
@@ -101,6 +109,7 @@ const appData = {
     logger: function() {
         console.log(appData.fullPrice);
         console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
         // for(const key in appData) {
         //     console.log(key, appData[key]);
         // }
