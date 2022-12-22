b34809980250ae35dd82fbac25f5797450321e5c
Mur <murmavilink@gmail.com>
2022-12-22 18:34:39 +0600

    урок 16 выполнено

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
 'use strict';
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.text = '';
-};
 
 
-
-//блок описания метода
-DomElement.prototype.createElement = function () {
-    let element;
-
-    if(this.selector[0] === '.') {
-        element = document.createElement('div');
-        element.classList.add(this.selector.slice(1));
-        this.text = `Это блок с классом ${this.selector}`;
-    } 
-
-    if(this.selector[0] === '#') {
-        element = document.createElement('p');
-        element.setAttribute('id', this.selector.slice(1));
-        this.text = `Это параграф с id ${this.selector}`;
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
     }
+}
 
-    element.style.cssText = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px;`;
-
-    element.textContent = this.text;
-
-    return element;
-};
-
-
-
-//блок функционала
-const newDiv = new DomElement('.block', 350, 650, 'red', 30);
-const newP = new DomElement('#paragraph', 80, 650, 'green', 28);
+class Second extends First {
+    hello() {
+        super.hello();
+        console.log("А я наследуемый метод!");
+    }
+}
 
 
-document.body.append(newDiv.createElement());
-document.body.append(newP.createElement());
+const obj = new Second();
+
+obj.hello();
